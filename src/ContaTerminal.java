3854a253fb6faa528794b48058de6f187aed5b8d
felipemrtn <luiz1997martins@gmail.com>
2024-05-04 15:00:14 -0300

    Texto atualizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -26,7 +26,7 @@ public class ContaTerminal {
 
         double saldo = scanner.nextDouble();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + "já está disponível para saque");        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");        
         
         scanner.close();
         
